0dc3eca2cf19943c7a7df62633ccf85e55e2d17a
Hofbrand <hofbrand.yauheni@gmail.com>
2023-02-12 11:23:59 +0700

    Init

---
+++ TestTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You've got 3 chests");
+            Console.WriteLine("1" + " 2" + " 3");
+            Console.WriteLine("Print number of chest which you would like to open");
+            var chestToOpen =Console.ReadLine();
+            Console.WriteLine($"You choose the chest number {chestToOpen}");
+        }
+    }
+}
